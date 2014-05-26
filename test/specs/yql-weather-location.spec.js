4d17e0124fd1c02193f9e8f3179c31a1e7e91f4a
ycao2 <ycao@walmartlabs.com>
2014-05-26 10:54:23 -0700

    init

--- test/specs/yql-weather-location.spec.js
+++ test/specs/yql-weather-location.spec.js
@@ -10,35 +10,36 @@ describe("yql-weather-location", function () {
             expect(res).to.be.ok;
             done();
           }, function (err) {
-            console.log(err);
             done();
           });
-
-//        YQL.exec("SELECT * FROM csv WHERE url='http://finance.yahoo.com/d/quotes.csv?s=FB&f=snl1d1t1ohgdr'", function (err, res) {
-//          expect(err).to.eql(null);
-//          expect(res).to.be.ok;
-//          done();
-//        });
-      });
-    });
-
-    describe("weather", function () {
-      it("should return weather info", function(done){
-        YQL.weather({city: "mountain view"}, function (err, res) {
-          expect(err).to.eql(null);
-          expect(res).to.be.ok;
-          done();
-        });
       });
     });
 
     describe("location", function () {
       it("should return location info", function(done){
-        YQL.location("palo alto", function (err, res) {
-          expect(err).to.eql(null);
-          expect(res).to.be.ok;
-          done();
-        });
+        YQL.location("palo alto")
+          .then(function (res) {
+            expect(res).to.be.ok;
+            done();
+          });
+      });
+    });
+
+    describe("weather", function () {
+      it("should return weather info when city is set", function(done){
+        YQL.weather({city: "mountain view"})
+          .then(function (res) {
+            expect(res).to.be.ok;
+            done();
+          });
+      });
+
+      it("should return weather info when woeid is set", function(done){
+        YQL.weather({woeid: "2455920"})
+          .then(function (res) {
+            expect(res).to.be.ok;
+            done();
+          });
       });
     });
 
